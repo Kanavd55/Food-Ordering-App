3584e65df1aedee83418d5ddcf90dcf55469b8d9
Kanav Dahat <kanavdahat55@gmail.com>
2023-08-19 01:20:56 +0530

    parcel is added to the app

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import  ReactDOM  from "react-dom";
+
+const AppLayout=()=>{
+    return(
+        <div>
+            hi
+        </div>
+    )
+}
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<AppLayout/>)
